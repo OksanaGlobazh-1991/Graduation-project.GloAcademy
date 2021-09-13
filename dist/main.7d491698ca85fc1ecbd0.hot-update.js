183b9341efb6fd2436ef9a1d29e8ce7d1f110d6e
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-13 18:37:57 +0300

    modalWindow is ready

---
+++ dist/main.7d491698ca85fc1ecbd0.hot-update.js
@@ -0,0 +1,40 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateDiplom"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_modalWindow__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modalWindow */ \"./src/modules/modalWindow.js\");\n\n(0,_modules_modalWindow__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n//# sourceURL=webpack://Diplom/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/modalWindow.js":
+/*!************************************!*\
+  !*** ./src/modules/modalWindow.js ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar modalWindow = function modalWindow() {\n  var callbackBtn = document.querySelectorAll('.callback-btn'),\n      modalCallback = document.querySelector('.modal-callback'),\n      modalOverlay = document.querySelector('.modal-overlay'),\n      modalClose = document;\n  callbackBtn.forEach(function (elem) {\n    elem.addEventListener('click', function () {\n      modalCallback.style.display = 'block';\n      modalOverlay.style.display = 'block';\n    });\n  });\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modalWindow);\n\n//# sourceURL=webpack://Diplom/./src/modules/modalWindow.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("d5c3da34e25c1a86e7fb")
+/******/ })();
+/******/ 
+/******/ }
+);
--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -1,13 +1,26 @@
 const modalWindow = () => {
-  const callbackBtn = document.querySelector('.callback-btn'),
+  const callbackBtn = document.querySelectorAll('.callback-btn'),
         modalCallback = document.querySelector('.modal-callback'),
-        modalOverlay = document.querySelector('.modal-overlay');
+        modalOverlay = document.querySelector('.modal-overlay'),
+        modalClose = document.querySelector('.modal-close');
 
-        callbackBtn.addEventListener('click', () => {
+        callbackBtn.forEach((elem) => {
+          elem.addEventListener('click', () => {
             modalCallback.style.display = 'block';
             modalOverlay.style.display = 'block';
           })
+        });
 
-};
-
+        document.addEventListener('click', (event) => {
+          let target = event.target;
+          if (target.closest('.modal-close')) {
+            modalCallback.style.display = 'none';
+            modalOverlay.style.display = 'none';
+          }
+          if (target.closest('.modal-overlay')) {
+            modalCallback.style.display = 'none';
+            modalOverlay.style.display = 'none';
+          }
+        })
+}
 export default modalWindow;
