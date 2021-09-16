761abfc8aaeb02776606b251860408cb6d77f6ff
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-16 14:38:24 +0300

    remove dist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import modalWindow from './modules/modalWindow';
 import servicePrices from './modules/servicePrices';
 import slider from './modules/slider';
 import ourServices from './modules/ourServices';
-import accordeon from './modules/accordeon';
+import newAccordeon from './modules/newAccordeon';
 
 
 modalWindow();
 servicePrices();
 slider();
 ourServices();
-accordeon();
+newAccordeon();
