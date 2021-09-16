f3c044bcdd79c68e2a7aa8f4d829122a25d74af4
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-09-16 23:22:58 +0300

    dfgh

---
+++ dist/main.bb76ac249d226d97ebe4.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateDiplom"]("main",{
+
+/***/ "./src/modules/sendForm.js":
+/*!*********************************!*\
+  !*** ./src/modules/sendForm.js ***!
+  \*********************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\src\\\\modules\\\\sendForm.js: Unterminated string constant. (8:71)\\n\\n\\u001b[0m \\u001b[90m  6 |\\u001b[39m   \\u001b[36mconst\\u001b[39m form \\u001b[33m=\\u001b[39m document\\u001b[33m.\\u001b[39mquerySelector(\\u001b[32m'form'\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  7 |\\u001b[39m   \\u001b[36mconst\\u001b[39m statusMessage \\u001b[33m=\\u001b[39m document\\u001b[33m.\\u001b[39mcreateElement(\\u001b[32m'div'\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m  8 |\\u001b[39m   \\u001b[36mconst\\u001b[39m formName \\u001b[33m=\\u001b[39m document\\u001b[33m.\\u001b[39mquerySelectorAll(\\u001b[32m'.input[name=\\\"user_name\\\"]'\\u001b[39m\\u001b[32m');\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m                                                                        \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  9 |\\u001b[39m   statusMessage\\u001b[33m.\\u001b[39mstyle\\u001b[33m.\\u001b[39mcssText \\u001b[33m=\\u001b[39m \\u001b[32m'font-size: 2rem; color: black'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 11 |\\u001b[39m   \\u001b[36mlet\\u001b[39m body\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Parser._raise (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:808:17)\\n    at Parser.raiseWithData (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:801:17)\\n    at Parser.raise (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:762:17)\\n    at Parser.readString (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:2651:20)\\n    at Parser.getTokenFromCode (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:2287:14)\\n    at Parser.nextToken (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1760:12)\\n    at Parser.next (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1666:10)\\n    at Parser.parseLiteralAtNode (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11732:10)\\n    at Parser.parseLiteral (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11738:17)\\n    at Parser.parseStringLiteral (D:\\\\JS\\\\MAMP\\\\htdocs\\\\Diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11742:17)\");\n\n//# sourceURL=webpack://Diplom/./src/modules/sendForm.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c6d4760365e80e2ac48c")
+/******/ })();
+/******/ 
+/******/ }
+);
